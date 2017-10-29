043b0d31fad8bc1e63a25bb5057cefca9ea306b2
Hazem Hemied <hazem.hemied@gmail.com>
2017-10-30 01:02:08 +0200

    Remove conflict files which may exist

--- fixvm.py
+++ fixvm.py
@@ -91,18 +91,23 @@ def vmwareFix():
             os.mkdir(os.path.join('/lib/modules/', kernel_version, 'misc'))
         # Contine if the directory exist
         except FileExistsError:
-            pass
-        copy2('/usr/lib/vmware/modules/source/vmmon-only/vmmon.ko', os.path.join('/lib/modules/', kernel_version, 'misc'))
-        copy2('/usr/lib/vmware/modules/source/vmnet-only/vmnet.ko', os.path.join('/lib/modules/', kernel_version, 'misc'))
-        print(" Building VMWare modules ...")
-        # Recompiling VMWARE modules
-        os.system('depmod -a')
-        print("""
-        ****************************************
-        Please enjoy VMware and Fedora
+            try:
+                if os.path.exists(os.path.join('/lib/modules/', kernel_version, 'misc', '/usr/lib/vmware/modules/source/vmmon-only/vmmon.ko')):
+                        os.remove(os.path.join('/lib/modules/', kernel_version, 'misc', '/usr/lib/vmware/modules/source/vmmon-only/vmmon.ko'))
+                else:
+                    pass
+        finally:
+            copy2('/usr/lib/vmware/modules/source/vmmon-only/vmmon.ko', os.path.join('/lib/modules/', kernel_version, 'misc'))
+            copy2('/usr/lib/vmware/modules/source/vmnet-only/vmnet.ko', os.path.join('/lib/modules/', kernel_version, 'misc'))
+            print(" Building VMWare modules ...")
+            # Recompiling VMWARE modules
+            os.system('depmod -a')
+            print("""
+            ****************************************
+            Please enjoy VMware and Fedora
 
-        Now, you can try to rerun VMware
-        """)
+            Now, you can try to rerun VMware
+            """)
 
 
 vmwareFix()
